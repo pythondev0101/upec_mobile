2a8c1651107593cd5e22bc5e8730a08b0d057913
rmontemayor <36463571+pythondev0101@users.noreply.github.com>
2022-09-10 06:23:35 +0800

    update: prod url

--- lib/utilities/url_utility.dart
+++ lib/utilities/url_utility.dart
@@ -1,6 +1,6 @@
 class AppUrls {
-  // static const String serverURL = "http://13.212.16.21";
-  static const String serverURL = "http://192.168.100.6:5000";
+  static const String serverURL = "http://54.254.131.228";
+  // static const String serverURL = "http://192.168.100.6:5000";
 
   static const String loginURL = "$serverURL/api/auth/login";
 
